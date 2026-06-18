8eb990dfd2146bcc2edaa0415662df12b7d895b2
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-18 23:14:33 +0530

    form added

---
+++ frontend/src/components/TaskForm.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+
+function TaskForm({ fetchTasks }) {
+  const [title, setTitle] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title) return;
+
+    try {
+      await axios.post(
+        "http://localhost:4000/api/tasks",
+        {
+          title,
+          dueDate,
+        }
+      );
+
+      setTitle("");
+      setDueDate("");
+
+      fetchTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter Task"
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)
+        }
+      />
+
+      <input
+        type="datetime-local"
+        value={dueDate}
+        onChange={(e) =>
+          setDueDate(e.target.value)
+        }
+      />
+
+      <button type="submit">
+        Add Task
+      </button>
+    </form>
+  );
+}
+
+export default TaskForm;
