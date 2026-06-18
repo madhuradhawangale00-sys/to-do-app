4b651e31540ba0ead2df1422fcb0ad759c5bbd8a
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-18 22:31:49 +0530

    routes added

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const {
+    getTasks,
+    createTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/taskController');
+
+const router = express.Router();
+
+router.get('/', getTasks);
+
+router.post('/', createTask);
+
+router.patch('/:id', updateTask);
+
+router.delete('/:id', deleteTask);
+
+module.exports = router;
